7641ca82aa94838f5aee8db5bc696284549d2d5a
Pratik Rai <info@netzaxis.com>
2020-01-16 11:10:35 +0000

    Set sample cookie on the client

--- request_handler.js
+++ request_handler.js
@@ -1,3 +1,17 @@
-const requestHandler = (req, res) => res.send('Hello World!');
+const requestHandler = (req, res) => {
+	// read cookies
+	console.log(req.cookies);
+
+	let options = {
+		maxAge: 1000 * 60 * 15, // would expire after 15 minutes
+		sameSite: 'lax',
+		httpOnly: true // The cookie only accessible by the web server
+		// signed: true // Indicates if the cookie should be signed
+	};
+
+	// Set cookie
+	res.cookie('same-site-cookie', 'foo', options); // options is optional
+	res.send('Hello World!');
+};
 
 module.exports = requestHandler;
