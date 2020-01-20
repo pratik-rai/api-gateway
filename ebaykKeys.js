6eeaaa93c0113606e8be98a5114e8c3ea2474255
Pratik Rai <info@netzaxis.com>
2020-01-20 10:12:34 +0000

    Gather all ebayk keys

---
+++ ebaykKeys.js
@@ -0,0 +1,15 @@
+const ebayKeys = {
+	kw: 'kw',
+	cat: 'cat',
+	tcat: 'tcat',
+	page: 'page',
+	g_pn: 'g_pn',
+	lsk: 'lsk',
+	lsc: 'lsc',
+	gam_count: 'gam_count',
+	location: 'location',
+	yo_app: 'yo_app',
+	cachebuster: 'cachebuster'
+};
+
+module.exports = ebayKeys;
