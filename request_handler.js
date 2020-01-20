1693ff9d0303ec97df38604fd7504eee2aef4ad0
Pratik Rai <info@netzaxis.com>
2020-01-20 10:14:58 +0000

    Send Treebay API response back to the client

--- request_handler.js
+++ request_handler.js
@@ -1,18 +1,18 @@
-const requestHandler = (req, res) => {
-	console.log(req.body);
-	// read cookies
-	console.log(req.cookies);
+const apiCaller = require('./apicaller');
 
-	let options = {
-		maxAge: 1000 * 60 * 15, // would expire after 15 minutes
-		sameSite: 'lax',
-		httpOnly: true // The cookie only accessible by the web server
-		// signed: true // Indicates if the cookie should be signed
-	};
+const requestHandler = async (req, res) => {
+	const { body } = req;
+	const data = await apiCaller(body);
+	// let options = {
+	// 	maxAge: 1000 * 60 * 15, // would expire after 15 minutes
+	// 	sameSite: 'lax',
+	// 	httpOnly: true // The cookie only accessible by the web server
+	// 	// signed: true // Indicates if the cookie should be signed
+	// };
 
-	// Set cookie
-	res.cookie('same-site-cookie', 'foo', options); // options is optional
-	res.send('Hello World!');
+	// // Set cookie
+	// res.cookie('same-site-cookie', 'foo', options); // options is optional
+	await res.json(data);
 };
 
 module.exports = requestHandler;
